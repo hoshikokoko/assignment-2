1694bdfd79a9bcd48715c0458bcf78da337cffb3
Ayano Hoshi <114926497+hoshikokoko@users.noreply.github.com>
2022-11-11 02:35:15 +0000

    [Add]3-13 新規登録機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def create
     book = Book.new(book_params)
     book.save
-    redirect_to book_path(book.id)
+    redirect_to books_path
   end
   
   def new
-    @book = Book.new
   end
   
   def edit
@@ -25,10 +25,9 @@ class BooksController < ApplicationController
   def destroy
   end
   
-  private
   
+  private
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
 end
